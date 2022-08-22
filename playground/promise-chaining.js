713706e169ba074595b3c98ef4d8b9ec37a00cd6
brian_lin <lcylcy66666@gmail.com>
2022-08-22 21:46:02 +0800

    Async/Await

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,24 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('62ffe98d090d561339be77f3',{age: 1}).then((user)=>{
-    console.log(user)
-    return User.countDocuments({ age: 1})
-}).then((result)=>{
-    console.log(result)
+// User.findByIdAndUpdate('62ffe98d090d561339be77f3',{age: 1}).then((user)=>{
+//     console.log(user)
+//     return User.countDocuments({ age: 1})
+// }).then((result)=>{
+//     console.log(result)
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+
+const updateAgeAndCount = async(id, age)=>{
+    const user = await User.findByIdAndUpdate(id, {age: age})
+    const count = await User.countDocuments({age})
+    return count 
+}
+
+updateAgeAndCount('62ffe98d090d561339be77f3', 2).then((count)=>{
+    console.log(count)
 }).catch((e)=>{
-    console.log(e)
+
 })
