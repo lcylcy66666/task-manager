feeb4a585e26996c6477d8e2ecef69cded8fb360
brian_lin <lcylcy66666@gmail.com>
2022-08-24 18:51:11 +0800

    app.xxx to router.xxx and seperate task and user in differnt file make code clean

--- src/index.js
+++ src/index.js
@@ -1,76 +1,16 @@
 const express = require("express")
 require('./db/mongoose')
-const User = require('./models/user')
 const Task = require('./models/task')
+const userRouter = require('./routers/user') 
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
+//Use express.Router() in routers file
+app.use(userRouter)
 
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-
-    /* user.save().then(()=>{
-        res.status(201).send(user)
-    }).catch((e)=>{
-        res.status(400).send(e)
-        res.send(e)
-    }) */
-})
-
-app.get('/users', async (req, res) => {
-
-    try {
-        const users = await User.find({})
-    } catch (e) {
-        res.status(500).send()
-    }
-
-    // User.find({}).then((users) => {
-    //     res.send(users)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-
-    // User.findById(_id).then((user) => {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-    //     res.send(user)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-    // console.log(req.params)
-})
-
-//Goal: Setup the task reading endpoints
-//
-//1. Create an endpoint for fetching all tasks
-//2. Create an endpoint for fetching a task by its id
-//3. Setup new requests in Postman and test your work
 
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
@@ -82,52 +22,71 @@ app.post('/tasks', async (req, res) => {
         res.status(400).send(e)
     }
 
-    // task.save().then(() => {
-    //     res.status(201).send(task)
-    // }).catch((e) => {
-    //     res.status(400).send(e)
-    // })
 })
 
 app.get('/tasks', async (req, res) => {
-    try { 
+    try {
         const tasks = await Task.find({})
         res.send(tasks)
     } catch (e) {
         res.status(500).send()
     }
 
-    // Task.find({}).then((tasks) => {
-    //     res.send(tasks)
-    // }).catch((e) => {
-    //     res.status(500).send(e)
-    // })
 })
 
 app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
 
-    try{
+    try {
         const task = await Task.findById(_id)
 
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: "Invalid updates!" })
+    }
+
+    try {
+        //"new" is going to return the new user as opposed to the existing one htat was found before update
+        //runValidator is going to make sure we do run validation for the update
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        
         if(!task){
             return res.status(404).send()
         }
 
         res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res)=>{
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task){
+            res.status(404).send()
+        }
+        res.send(task)
     }catch(e){
         res.status(500).send()
     }
-
-    // Task.findById(_id).then((task) => {
-    //     if (!task) {
-    //         return res.status(404).send()
-    //     }
-    //     res.send(task)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
 })
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
